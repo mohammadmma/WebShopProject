f90685fb25c0a217fe82005fc56b9aa39801f309
mohammad mma <aslmohammad10@gmail.com>
2023-01-28 15:32:19 +0330

    init function is written

--- WSproducts.py
+++ WSproducts.py
@@ -1,2 +1,6 @@
 class Product:
-    pass
+    def __init__(self, name, id, image, description):
+        self.name = name
+        self.id = id
+        self.image = image
+        self.description = description
