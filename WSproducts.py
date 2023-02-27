e110d372b6c61f83a5dc213d79fe9a06195329ac
mohammad mma <aslmohammad10@gmail.com>
2023-02-27 21:08:07 +0330

    minor changes

--- WSproducts.py
+++ WSproducts.py
@@ -1,26 +1,26 @@
 class Product:
-    def __init__(self, name, id, image, description, num_of_prodocut):
+    def __init__(self, name, id, image, description, quantity):
         self.name = name
         self.id = id
         self.image = image
         self.description = description
-        self.num_of_product = num_of_prodocut
+        self.quantity = quantity
 
     def increasing_the_amount(self, number):
-        self.num_of_product += number
-        print("the available number of this product is", self.num_of_product)
+        self.quantity += number
+        print("the available number of this product is", self.quantity)
 
     def decreasing_the_amount(self, number):
-        self.num_of_product -= number
-        if self.num_of_product < 0:  # check the amount of product #
+        self.quantity -= number
+        if self.quantity < 0:  # check the amount of product #
             print("Sorry the product is unavailable")
             raise ValueError
         else:
-            print("the available number of this product is", self.num_of_product)
+            print("the available number of this product is", self.quantity)
 
     def __str__(self):
         return f"this product is {self.name}, with id {self.id}, {self.description}, and the available amount is " \
-               f"{self.num_of_product}"
+               f"{self.quantity}"
 
 
 # creating user object with specification username and password
@@ -37,4 +37,4 @@ print(product0)
 product0.decreasing_the_amount(1)
 product0.increasing_the_amount(20)
 product0.decreasing_the_amount(21)
-product0.decreasing_the_amount(1)
+# product0.decreasing_the_amount(1)   # this line is for testing value error
